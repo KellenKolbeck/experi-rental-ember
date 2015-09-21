b8240625bece475f831b59238151c2683b45e9ca
Kellen Kolbeck & Kyle Mellander <student@epicodus.com>
2015-09-21 11:51:20 -0700

    Add lots of stuff

---
+++ app/components/rental-tile.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      this.sendAction('destroyRental', rental);
+    }
+  }
+});
